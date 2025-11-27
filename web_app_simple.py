ef3d5db1630d05aa2c30664c22619835b5d0e7b0
proschinesi <proschinesi2010@gmail.com>
2025-11-27 23:01:32 +0100

    Aggiornato web_app_simple.py per usare variabile PORT da environment (Railway)

--- web_app_simple.py
+++ web_app_simple.py
@@ -788,7 +788,8 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--host', default='0.0.0.0')
-    parser.add_argument('--port', type=int, default=5000)
+    # Usa PORT da environment (Railway, Heroku, etc.) o default 5000
+    parser.add_argument('--port', type=int, default=int(os.environ.get('PORT', 5000)))
     args = parser.parse_args()
     
     print(f"\n=== Server Web Semplificato ===")
